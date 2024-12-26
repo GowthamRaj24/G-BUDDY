eb695c3bb34ba203932b309c00896b5cff619652
Gowtham Raj <gmanda@gitam.in>
2024-12-27 01:13:48 +0530

    checking deploy

--- gbuddy_backend/driveAPI/uploadToDrive.js
+++ gbuddy_backend/driveAPI/uploadToDrive.js
@@ -1,35 +1,27 @@
-const fs = require('fs');
-const driveService = require('./googleDrive');
-
 const uploadToDrive = async (fileObject) => {
     const fileMetadata = {
-        name: fileObject.originalname,
-        parents: ['1_Hn39XjOA9vu9fhjYTz6Vm37MwYqDWVg']
+      name: fileObject.originalname,
+      parents: ['1_Hn39XjOA9vu9fhjYTz6Vm37MwYqDWVg']
     };
-
+  
     const media = {
-        mimeType: fileObject.mimetype,
-        body: fs.createReadStream(fileObject.path)
+      mimeType: fileObject.mimetype,
+      body: Buffer.from(fileObject.buffer) // Use buffer directly
     };
-
+  
     try {
-        const response = await driveService.files.create({
-            resource: fileMetadata,
-            media: media,
-            fields: 'id, webViewLink',
-            supportsAllDrives: true,
-        });
-        
-
-        console.log("res-- " + response.data.id)
-
-        const fileUrl = `https://drive.google.com/uc?id=${response.data.id}`;
-        fs.unlinkSync(fileObject.path);
-        return fileUrl;
+      const response = await driveService.files.create({
+        resource: fileMetadata,
+        media: media,
+        fields: 'id, webViewLink',
+        supportsAllDrives: true,
+      });
+  
+      const fileUrl = `https://drive.google.com/uc?id=${response.data.id}`;
+      return fileUrl;
     } catch (error) {
-        console.log("error in uploading--" , error.message);
-        throw new Error(`Drive API Error: ${error.message}`);
+      console.log("error in uploading--", error.message);
+      throw new Error(`Drive API Error: ${error.message}`);
     }
-};
-
-module.exports = uploadToDrive;
+  };
+  
--- gbuddy_backend/routes/notes.js
+++ gbuddy_backend/routes/notes.js
@@ -1,12 +1,6 @@
 const express = require("express");
 const multer = require('multer');
-const storage = multer.diskStorage({
-    destination: 'uploads/',
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname)
-    }
-});
-
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 const middleware = require("../middleware/auth.js");
 
