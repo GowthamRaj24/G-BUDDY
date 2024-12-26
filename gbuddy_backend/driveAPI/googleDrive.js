96e56967b8e5c328e7ac99e988f422b76fd82899
Gowtham Raj <gmanda@gitam.in>
2024-12-26 12:24:01 +0530

    fixing issues

--- gbuddy_backend/driveAPI/googleDrive.js
+++ gbuddy_backend/driveAPI/googleDrive.js
@@ -1,20 +1,29 @@
-const { GoogleAuth } = require('google-auth-library');
 const { google } = require('googleapis');
+const https = require('https');
+require('dotenv').config();
+const credentials = {
+    client_id: process.env.CLIENT_ID,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.CLIENT_EMAIL,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+    universal_domain: process.env.UNIVERSAL_DOMAIN
+  };
 
-// Use the newer GoogleAuth implementation
-const auth = new GoogleAuth({
-    credentials: {
-        client_email: process.env.CLIENT_EMAIL,
-        private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-        client_id: process.env.CLIENT_ID
-    },
+  
+
+const auth = new google.auth.GoogleAuth({
+    credentials,
     scopes: ['https://www.googleapis.com/auth/drive'],
-    projectId: process.env.PROJECT_ID
+    clientOptions: {
+      crypto: { useOpenSSL: false }
+  }
 });
 
-const driveService = google.drive({ 
-    version: 'v3', 
-    auth: auth.getClient()
-});
+const driveService = google.drive({ version: 'v3', auth});
 
 module.exports = driveService;
