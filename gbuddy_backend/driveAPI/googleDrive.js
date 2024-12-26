d347a1c1ffac62e5868b156901999ec8e7ccf7c2
Gowtham Raj <gmanda@gitam.in>
2024-12-26 12:16:51 +0530

    fixing issues

--- gbuddy_backend/driveAPI/googleDrive.js
+++ gbuddy_backend/driveAPI/googleDrive.js
@@ -1,23 +1,20 @@
+const { GoogleAuth } = require('google-auth-library');
 const { google } = require('googleapis');
-require('dotenv').config();
 
-const auth = new google.auth.JWT(
-    process.env.CLIENT_EMAIL,
-    null,
-    process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-    ['https://www.googleapis.com/auth/drive'],
-    null
-);
+// Use the newer GoogleAuth implementation
+const auth = new GoogleAuth({
+    credentials: {
+        client_email: process.env.CLIENT_EMAIL,
+        private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+        client_id: process.env.CLIENT_ID
+    },
+    scopes: ['https://www.googleapis.com/auth/drive'],
+    projectId: process.env.PROJECT_ID
+});
 
 const driveService = google.drive({ 
     version: 'v3', 
-    auth,
-    timeout: 15000,
-    // Force using Node's native crypto instead of OpenSSL
-    cryptoOptions: {
-        algorithm: 'RS256',
-        useNativeCrypto: true
-    }
+    auth: auth.getClient()
 });
 
 module.exports = driveService;
