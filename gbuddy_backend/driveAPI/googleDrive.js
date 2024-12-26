50fa990786f19573b3321f6933f80e1f27f5e4ae
Gowtham Raj <gmanda@gitam.in>
2024-12-26 12:46:53 +0530

    fixing issudwades

--- gbuddy_backend/driveAPI/googleDrive.js
+++ gbuddy_backend/driveAPI/googleDrive.js
@@ -5,7 +5,7 @@ const credentials = {
     client_id: process.env.CLIENT_ID,
     project_id: process.env.PROJECT_ID,
     private_key_id: process.env.PRIVATE_KEY_ID,
-    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    private_key: process.env.PRIVATE_KEY.split(String.raw`\n`).join('\n'),
     client_email: process.env.CLIENT_EMAIL,
     auth_uri: process.env.AUTH_URI,
     token_uri: process.env.TOKEN_URI,
